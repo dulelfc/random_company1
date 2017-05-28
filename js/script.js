be79373e6b40b64baf0b696b68331413858b5d66
Dusan Jeremic <Dusan Jeremic>
2017-05-28 19:42:22 +0200

    Final

---
+++ js/script.js
@@ -0,0 +1,13 @@
+new WOW().init();
+
+$(document).ready(
+    function(){
+        $('html').niceScroll({
+            cursorwidth: '8',
+            cursorborderradius: 'none',
+            cursorborder: 'none',
+            cursorcolor: '#3498db',
+            mousescrollstep: '60'
+        });
+    }
+);
